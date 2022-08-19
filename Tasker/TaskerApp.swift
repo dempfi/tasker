379536c409ecc3c352e4e432703344edf37cd593
Ike Kurghinyan <mail@dempfi.com>
2022-08-19 23:12:33 +0400

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Ike Ku on 19.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
